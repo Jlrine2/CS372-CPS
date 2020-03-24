34ff4b168367875a0d0c4297e69ab95bdce815e8
BuildTools <nickalexeev@protonmail.com>
2020-03-24 14:50:45 -0800

    Initial template for classes

--- src/cps.hpp
+++ src/cps.hpp
@@ -4,11 +4,30 @@
 
 #ifndef CS372_CPS_CPS_HPP
 #define CS372_CPS_CPS_HPP
+#include <iostream>
 
-
-class cps {
+class Shape {
+    public:
+    ~Shape();
+    Shape();
+    virtual std::iostream draw()=0;  
+};
+class Polygon: Shape{
 
 };
+class Square: Shape{
 
+};
+class Rectangle: Shape{
 
+};
+class Triangle: Shape{
+
+};
+class Circle: Shape{
+
+};
+class Spacer: Shape{
+
+};
 #endif //CS372_CPS_CPS_HPP
